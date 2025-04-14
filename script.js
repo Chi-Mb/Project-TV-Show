a620a306e177076bd0acb0d7d3e80e02be772e2f
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-04-14 12:29:54 +0100

    Displayed all 73 episodes with name, image, season/episode, and description

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+for (const episode of episodeList) {
+  console.log(episode)
+  const newdiv = document.createElement("div")
+  const newimg = document.createElement("img")
+  newimg.src = `${episode.image.medium}`
+  const description = document.createElement("div")
+  rootElem.appendChild(newdiv)
+  rootElem.appendChild(newimg)
+  rootElem.appendChild(description)
+  newdiv.textContent = `${episode.name} - S${episode.season.toString().padStart(2,0)} E${episode.number.toString().padStart(2,0)}`
+  description.textContent = `${episode.summary}`
+}
+
+
 }
 
 window.onload = setup;
